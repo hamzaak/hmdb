62f95c0cd61c48a30af24bed23179078ab96d1a6
Hamza Ak <akhamza88@gmail.com>
2023-03-18 22:42:27 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ön Gereksinimler
+1. NodeJs ve NPM kurulumu
+2. create-react-app kurulumu
+3. npx create-react-app sample-app
+
+
+https://medium.com/@nagireddygajjela19/how-to-create-hello-world-react-application-d907e46ad325
+hazır api listesi --> https://www.themoviedb.org/
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World :)
+          Hello again :)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
