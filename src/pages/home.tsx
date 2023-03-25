42405469e06967aa4bba31e08b0785c8ca8b2678
Hamza Ak <akhamza88@gmail.com>
2023-03-25 22:42:12 +0300

    intro title position refactored

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './home.css';
-import { Text, Stack, Container, Image, Overlay, Title, Rating, Grid } from "@mantine/core";
+import { Text, Stack, Container, Image, Overlay, Title, Rating, Grid, MediaQuery, Divider } from "@mantine/core";
 import { Carousel } from '@mantine/carousel';
 import { tmdbKey, tmdbBaseUrl, tmdbImageBaseUrl } from '../config';
 import { Movie } from '../models/movie';
@@ -25,7 +25,7 @@ export default class Home extends React.Component<IHomeProps, IHomeState> {
       }
     
     componentDidMount() {
-        axios.get(`${tmdbBaseUrl}/movie/now_playing?api_key=${tmdbKey}`)
+        axios.get(`${tmdbBaseUrl}/movie/now_playing?api_key=${tmdbKey}&page=1`)
             .then(res => {
                 const movies = res.data.results;
                 const latestMovie = res.data.results[0];
@@ -57,27 +57,40 @@ export default class Home extends React.Component<IHomeProps, IHomeState> {
                     </Grid.Col>
                 </Grid>
 
-                <Overlay opacity={0} style={{marginLeft: '7rem'}}>
-                    <Stack align="flex-start" justify="center" style={{height: '100%', width: '50%'}}>
+                <MediaQuery smallerThan="lg" styles={{ display: 'none' }}>
+                    <Overlay opacity={0} style={{marginLeft: '7rem'}}>
+                        <Stack align="flex-start" justify="center" style={{height: '100%', width: '50%'}}>
+                            <Title order={2}>{this.state.latestMovie?.original_title}</Title>
+                            <Rating value={this.state.latestMovie?.vote_average ? this.state.latestMovie?.vote_average / 2 : 0} fractions={50} readOnly/>
+                            <Text ta="left" fz="lg">
+                                {this.state.latestMovie?.overview}
+                            </Text>
+                            <Divider my="sm" />
+                        </Stack>
+                    </Overlay>
+                </MediaQuery>
+                
+                <MediaQuery largerThan="md" styles={{ display: 'none' }}>
+                    <Stack mt={20} align="flex-start" justify="center" style={{height: '100%', width: '100%'}}>
                         <Title order={2}>{this.state.latestMovie?.original_title}</Title>
                         <Rating value={this.state.latestMovie?.vote_average ? this.state.latestMovie?.vote_average / 2 : 0} fractions={50} readOnly/>
                         <Text ta="left" fz="lg">
                             {this.state.latestMovie?.overview}
                         </Text>
                     </Stack>
-                </Overlay>
+                </MediaQuery>
 
-                <Stack>
-                    <Text fz="xl">Now playing movies</Text>
+                <Stack mt={20}>
+                    <Title order={2}>Now playing movies</Title>
                     <Carousel
-                        height={480}
+                        height={440}
                         slideSize="20%"
                         slideGap="xs"
                         align="start"
                         slidesToScroll={5}>
                         {this.state.nowPlayingMovies?.map(function(movie, index){
                             return <Carousel.Slide key={index}>
-                                <Stack spacing={10}>
+                                <Stack spacing={5}>
                                     {
                                         movie.backdrop_path && 
                                         (
