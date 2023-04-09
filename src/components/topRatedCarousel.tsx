27105b77b710848fa7c9d0c9fd7330a5368ca5bc
Hamza Ak <akhamza88@gmail.com>
2023-04-10 02:25:54 +0300

    home page carousel optimized

---
+++ src/components/topRatedCarousel.tsx
@@ -0,0 +1,67 @@
+import { Text, Stack, Grid } from "@mantine/core";
+import { Carousel } from '@mantine/carousel';
+import MovieItem from './movieItem';
+import { Link } from 'react-router-dom';
+import { useCurrentWidth } from "../hooks/useCurrentWidth";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { useEffect } from "react";
+import { selectTopRatedMovies } from "../store/movie/top-rated/selectors";
+import { fetchTopRatedMovies } from "../store/movie/top-rated/actions";
+
+export default function TopRatedCarousel () {
+    const movies = useAppSelector(selectTopRatedMovies);
+    const dispatch = useAppDispatch();
+    let s_width = useCurrentWidth();
+
+    useEffect(() => {
+
+        if (movies.length === 0) {
+            dispatch(fetchTopRatedMovies());
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return(
+        <Stack mt={30}>
+            <Grid>
+                <Grid.Col span="content">
+                    <Text fz="lg">Top Rated Movies </Text>
+                </Grid.Col>
+                <Grid.Col span="content">
+                    <Text fz="lg">
+                        <Link to={'/top_rated'} style={{color: '#e03131'}}>Explore all</Link>
+                        
+                    </Text>
+                </Grid.Col>
+            </Grid>
+            
+            <Carousel
+                mr={30}
+                height={
+                    s_width < 480 ? 190 : (
+                        s_width < 768 ? 240 : (
+                            s_width < 1024 ? 300: 380
+                        )) }
+                slideSize={
+                    s_width < 480 ? "100%" : (
+                        s_width < 768 ? "50%" : (
+                            s_width < 1024 ? "33.33%": "16.66%"
+                        )) }
+                slideGap="xs"
+                align="start"
+                slidesToScroll={
+                    s_width < 480 ? 1 : (
+                        s_width < 768 ? 2 : (
+                            s_width < 1024 ? 3 : 6
+                        )
+                    )}>
+                { movies && movies.slice(0, 15).map(function(movie, index){
+                    return <Carousel.Slide key={index}>
+                            <MovieItem movie={movie} />
+                        </Carousel.Slide>;
+                })}
+            </Carousel>
+        </Stack>
+    ) 
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,43 +2,24 @@ import { useEffect, lazy, Suspense } from "react";
 import { useAppSelector, useAppDispatch } from '../store/hooks';
 import './home.css';
 import { Container } from "@mantine/core";
-import { selectNowPlayingMovies, selectNowPlayingMoviesLatestMovie } from "../store/movie/now-playing/selectors";
+import { selectNowPlayingMoviesLatestMovie } from "../store/movie/now-playing/selectors";
 import { fetchNowPlayingMovies } from "../store/movie/now-playing/actions";
-import { selectUpcomingMovies } from "../store/movie/upcoming/selectors";
-import { fetchUpcomingMovies } from "../store/movie/upcoming/actions";
-import { selectPopularMovies } from "../store/movie/popular/selectors";
-import { fetchPopularMovies } from "../store/movie/popular/actions";
-import { selectTopRatedMovies } from "../store/movie/top-rated/selectors";
-import { fetchTopRatedMovies } from "../store/movie/top-rated/actions";
 
 const LatestMovie = lazy(() => import('../components/latestMovie'));
-const MovieCarousel = lazy(() => import('../components/movieCarousel'));
+const NowPlayingCarousel = lazy(() => import('../components/nowPlayingCarousel'));
+const UpcomingCarousel = lazy(() => import('../components/upcomingCarousel'));
+const PopularCarousel = lazy(() => import('../components/popularCarousel'));
+const TopRatedCarousel = lazy(() => import('../components/topRatedCarousel'));
 
 export default function Home() {
-    const nowPlayingMovies = useAppSelector(selectNowPlayingMovies);
     const latestMovie = useAppSelector(selectNowPlayingMoviesLatestMovie);
-    const upcomingMovies = useAppSelector(selectUpcomingMovies);
-    const popularMovies = useAppSelector(selectPopularMovies);
-    const topRatedMovies = useAppSelector(selectTopRatedMovies);
 
     const dispatch = useAppDispatch();
 
     useEffect(() => {
 
-        if (nowPlayingMovies.length === 0) {
+        if (!latestMovie) {
             dispatch(fetchNowPlayingMovies());
-        }
-
-        if (upcomingMovies.length === 0) {
-            dispatch(fetchUpcomingMovies());
-        }
-
-        if (popularMovies.length === 0) {
-            dispatch(fetchPopularMovies());
-        }
-
-        if (topRatedMovies.length === 0) {
-            dispatch(fetchTopRatedMovies());
         }
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -51,31 +32,19 @@ export default function Home() {
             </Suspense>
 
             <Suspense>
-                <MovieCarousel
-                    category='now_playing'
-                    title='Now playing movies'
-                    movies={nowPlayingMovies.slice(0, 20)} />
+                <NowPlayingCarousel />
             </Suspense>
 
             <Suspense >
-                <MovieCarousel
-                    category='upcoming'
-                    title='Upcoming movies'
-                    movies={upcomingMovies.slice(0, 20)} />
+                <UpcomingCarousel />
             </Suspense>
 
             <Suspense >
-                <MovieCarousel
-                    category='popular'
-                    title='Popular movies'
-                    movies={popularMovies.slice(0, 20)} />
+                <PopularCarousel />
             </Suspense>
 
             <Suspense >
-                <MovieCarousel
-                    category='top_rated'
-                    title='Top rated movies'
-                    movies={topRatedMovies.slice(0, 20)} />
+                <TopRatedCarousel />
             </Suspense>
         </Container>
     )
